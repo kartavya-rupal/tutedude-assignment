11f99a015c87a3c609ead3fbb3c55ba51ad09e0d
kartavya-rupal <kartavyarupal.kr@gmail.com>
2026-04-05 15:36:00 +0530

    sockethandler group chat logic improvements

--- server/sockets/socketHandler.js
+++ server/sockets/socketHandler.js
@@ -1,14 +1,14 @@
 import { getDistance } from "../utils/distance.js";
 
 const users = {};
-
-const RADIUS = 80; 
+const RADIUS = 80;
 
 export default function handleSocket(io, socket) {
     users[socket.id] = {
         id: socket.id,
         x: 100,
         y: 100,
+        room: null,
     };
 
     io.emit("users:update", users);
@@ -19,37 +19,69 @@ export default function handleSocket(io, socket) {
         users[socket.id].x = x;
         users[socket.id].y = y;
 
-        const nearbyUsers = [];
+        const currentUser = users[socket.id];
+
+        const nearby = [];
 
         for (let id in users) {
             if (id === socket.id) continue;
 
-            const distance = getDistance(
-                x,
-                y,
-                users[id].x,
-                users[id].y
-            );
+            const other = users[id];
+
+            const distance = getDistance(x, y, other.x, other.y);
 
             if (distance < RADIUS) {
-                nearbyUsers.push(id);
+                nearby.push(id);
             }
         }
 
-        io.emit("users:update", users);
+        const cluster = [socket.id, ...nearby].sort();
 
-        socket.emit("nearby:users", nearbyUsers);
+        let newRoom = null;
+
+        if (cluster.length > 1) {
+            newRoom = `room-${cluster.join("-")}`;
+        }
+
+        if (currentUser.room !== newRoom) {
+            if (currentUser.room) {
+                socket.leave(currentUser.room);
+                console.log(`${socket.id} left ${currentUser.room}`);
+            }
+
+            if (newRoom) {
+                socket.join(newRoom);
+                console.log(`${socket.id} joined ${newRoom}`);
+            }
+
+            currentUser.room = newRoom;
+        }
+
+        io.emit("users:update", users);
+        socket.emit("nearby:users", nearby);
     });
 
-    socket.on("chat:message", ({ message, to }) => {
-        io.to(to).emit("chat:message", {
+    socket.on("chat:message", ({ message }) => {
+        const user = users[socket.id];
+        if (!user || !user.room) return;
+
+        io.to(user.room).emit("chat:message", {
             from: socket.id,
             message,
         });
     });
 
     socket.on("disconnect", () => {
+        const user = users[socket.id];
+
+        if (user?.room) {
+            socket.leave(user.room);
+        }
+
         delete users[socket.id];
+
         io.emit("users:update", users);
+
+        console.log("User disconnected:", socket.id);
     });
 }
